9a80ff9fdd9a94c32a0a0c3d67aacf4998e29958
Cellsito <celsobfigueroa@gmail.com>
2024-05-03 15:24:25 -0300

    Feat: Exercício 3

---
+++ ex3.js
@@ -0,0 +1,8 @@
+//Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let number = parseInt (prompt("Digite um número:"))
+
+if (number % 2 == 0) {
+    console.log("Esse número é par!")
+}else 
+    console.log("Esse número é ímpar!")
