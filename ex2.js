1abc3c78614f812693442fdda933e9347bdd3f03
Cellsito <celsobfigueroa@gmail.com>
2024-05-03 15:01:26 -0300

    Feat: Exercício 2

---
+++ ex2.js
@@ -0,0 +1,38 @@
+/* Escreva um programa que solicita ao usuário um número de 1 a 7 representando 
+um dia da semana e exibe o nome correspondente a esse dia.
+Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante. */
+
+let diasemana = parseInt (prompt ("Aperte um número de 1 a 7 numerar um dia da semana"))
+
+if (diasemana == 1) {
+    console.log("Segunda-Feira")
+    
+}else if (diasemana == 2) {
+    console.log("Terça-Feira")
+    
+}else if (diasemana == 3) {
+    console.log("Quarta-Feira")
+    
+}else if (diasemana == 4) {
+    console.log("Quinta-Feira")
+    
+}else if (diasemana == 5) {
+    console.log("Sexta-Feira")
+    
+}else if (diasemana == 6) {
+    console.log("Sábado")
+    
+}else if (diasemana == 7) {
+    console.log("Domingo")
+    
+}else
+    console.error("Esse número não consta!!!")
+    
+
+
+
+
+
+
+
+
