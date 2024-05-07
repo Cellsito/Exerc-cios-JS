aad1c92093b8eae4a978d6fe50cff5b71b35e75a
Cellsito <celsobfigueroa@gmail.com>
2024-05-07 17:05:19 -0300

    Feat: Exercício Loop 2

--- loop/loop1.js
+++ loop/loop1.js
@@ -1,4 +1,4 @@
-1. /* Imprimir números de 1 a 10 utilizando o loop for */
+ /* 1.Imprimir números de 1 a 10 utilizando o loop for */
 
 for (let cont = 1; cont <= 10 ; cont ++) {
     console.log(cont);
---
+++ loop/loop2.js
@@ -0,0 +1,7 @@
+/* 2. Imprimir números pares de 1 a 10. */
+
+for (let cont = 1; cont <= 10 ; cont ++) {
+    if (cont % 2 == 0) {
+    console.log(cont);
+    }    
+}
