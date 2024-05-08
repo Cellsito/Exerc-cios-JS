aa9b55012290a8d9ca3c174899fc8760985c7be3
Cellsito <celsobfigueroa@gmail.com>
2024-05-08 16:20:19 -0300

    Feat: Exercício Loop 9

---
+++ loop/loop9.js
@@ -0,0 +1,10 @@
+// 9. Faça um programa que receba um número do usuário e mostre no console a contagem regressiva até 0.
+
+let num = parseInt(prompt("Digite o primeiro número para contagem regressiva"))
+
+while (num >= 0) {
+    console.log(num)
+
+    num --
+    
+}
