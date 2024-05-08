87df4b6a294a3f606efa04b7c3602bac321855e9
Cellsito <celsobfigueroa@gmail.com>
2024-05-08 16:10:39 -0300

    Feat: Exercícios Loop 8

---
+++ loop/loop8.js
@@ -0,0 +1,12 @@
+// 8. Faça um programa que receba a idade do usuário, enquanto a
+// resposta do usuário for menor que 18 anos continue a solicitar a
+// idade e mostre uma mensagem quando a idade do usuário for maior que 18.
+
+let idade = parseInt(prompt("Qual a sua idade?"))
+
+while (idade < 18) {
+    idade = parseInt(prompt("Qual a sua idade?"))
+    
+}
+
+console.log("VOCÊ É MAIOR DE IDADEEE!!! :D");
